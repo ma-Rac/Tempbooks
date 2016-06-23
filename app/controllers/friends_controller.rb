093dddc8cb3d3b04ffdaf3c194df88b9e483b136
ma-Rac <mno.racca@gmail.com>
2016-06-23 12:16:18 -0300

    Added Friends Pages, Index and show, routed some paths

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,12 +1,16 @@
 class FriendsController < ApplicationController
   def create
+
     end
 
-  end
 
   def index
+    @friends = current_user.friends.joins(:profile).
+      order("profiles.name asc")
   end
 
   def show
+    @friend = Friend.find( params[:id] )
+    @profile = Profile.where(user_id: @friend.id)
   end
 end
--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,4 +1,5 @@
 class Friend < ActiveRecord::Base
   self.table_name = :users
   has_many :friendships
+  has_one :profile, foreign_key: :user_id
 end
